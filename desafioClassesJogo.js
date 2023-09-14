8529b2c0d3da3a3443cdafa9447513104eda1d77
AgataAraujo-dev <139137656+AgataAraujo-dev@users.noreply.github.com>
2023-09-14 17:42:39 -0300

    Add files via upload

---
+++ desafioClassesJogo.js
@@ -0,0 +1,33 @@
+class heroi {
+    constructor (nome, idade, tipo){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+    }
+
+    atacar(){
+       let ataque
+
+       switch (this.tipo) {
+        case 'Mago':
+            ataque = 'magia';
+            break;
+        case 'Guerreiro':
+            ataque = 'espada';
+            break;
+        case 'Monge':
+            ataque = 'artes marciais';
+            break;
+        case 'Ninja':
+            ataque = 'shuriken';
+            break;
+        default:
+            ataque = 'realizou um ataque';
+       }
+    console.log(`O ${this.tipo} atacou usando ${ataque}`)
+    }
+}
+
+const reiArthur = new heroi ("Rei Arthur",24 ,"Ninja")
+
+reiArthur.atacar()
